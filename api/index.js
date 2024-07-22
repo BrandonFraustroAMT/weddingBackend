4b2a7aa5e1b6bc6f0862d2689ff1af419db051c0
Brandon Fraustro <brandon.fraustro@almamartech.com>
2024-07-22 16:58:31 -0600

    feat: add vercel configuration

--- api/index.js
+++ api/index.js
@@ -28,8 +28,66 @@ const connect = async () => {
 connect();
 
 
-app.get('/api', async (req, res) => {
+app.get('/', async (req, res) => {
   res.send('Prueba')
+})
+
+app.get('/invitados', async (req, res) => {
+  try {
+    const result = await db.sql`
+    SELECT
+      i.id AS ID,
+      i.nombre AS invitado_nombre, 
+      i.apellido AS invitado_apellido, 
+      i.celular, 
+      i.nombrefamilia, 
+      i.niños, 
+      a.nombre AS acompanante_nombre, 
+      a.apellido AS acompanante_apellido,
+      i.confirmacion AS confirmacion
+    FROM 
+      acompañante AS a
+    FULL JOIN 
+      invitado AS i ON i.id = a.invitado_id;
+    `;
+    res.json(result);
+  } catch (error) {
+    console.error('Error al obtener los invitados:', error);
+    res.status(500).json({ error: 'Error al obtener los invitados' });
+  }
+})
+
+app.put('/invitados/:id', async (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const { 
+    nombre, apellido, celular, nombrefamilia, niños, confirmacion
+  } = req.body;
+  try {
+    const result = await db.sql`
+      UPDATE invitado
+      SET 
+        nombre = ${nombre},
+        apellido = ${apellido},
+        celular = ${celular},
+        nombrefamilia = ${nombrefamilia},
+        niños = ${niños},
+        confirmacion = ${confirmacion}
+      WHERE id = ${id};
+    `;
+    if (result.changes > 0) {
+      res.json({ message: 'Invitado actualizado correctamente' });
+    } else {
+      res.status(404).json({ error: 'Invitado no encontrado' });
+    }
+  } catch (error) {
+    console.error('Error al actualizar el invitado:', error);
+    res.status(500).json({ error: 'Error al actualizar el invitado' });
+  }
+})
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 })
 
 module.exports = app;
